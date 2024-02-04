4e2f1c45b6fdf9a2f10663efd402ca682f86671f
SANDEEPREYYI <sandeepreyyi2003@gmail.com>
2024-02-04 19:22:22 +0530

    fixes2.0

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -173,7 +173,7 @@ const Register = () => {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link to ="/signin" variant="body2">
+                <Link href="/signin" variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
